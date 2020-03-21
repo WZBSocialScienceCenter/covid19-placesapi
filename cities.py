41a6d4d44edef57a504bc7c2e27120806e2a107a
Markus Konrad <markus.konrad@wzb.eu>
2020-03-21 23:23:35 +0100

    fetching places of interest WIP

--- cities.py
+++ cities.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 cities = pd.read_csv('data/worldcities.csv')
-cities = cities.loc[cities.capital == 'primary', ['city', 'country', 'iso2', 'lat', 'lng']]
+cities = cities.loc[(cities.capital == 'primary') & (~cities.population.isna()) & (cities.population >= 100000),
+                    ['city', 'country', 'iso2', 'lat', 'lng']]
 
 cities.to_csv('data/cities.csv', index=False)
--- places.py
+++ places.py
@@ -1,4 +1,5 @@
-import logging
+import pickle
+import math
 from datetime import datetime
 
 import pandas as pd
@@ -7,102 +8,145 @@ import populartimes
 
 from apikeys import API_KEY
 
-logger = logging.getLogger('places')
-logger.addHandler(logging.NullHandler())
-logger.setLevel(logging.INFO)
-
 #%%
 
 PLACE_SEARCHES = [
     'restaurant',
     'bar',
-    'gastro pub',
     'club',
-    'main station',
+    'train station',
     'tourist information',
     'sights',
     'park',
     'mall',
     'supermarket',
-    'grocery store',
     'hardware store'
 ]
 
 PLACE_SEARCH_RADIUS = 3000  # in meters
+LIMIT_NUM_PLACES = 10
 
 RESULT_FILE = 'data/places_of_interest.csv'
+QUERIED_FILE = 'data/places_of_interest_queried_cities.pickle'
+
+#%%
+
+def haversine(a_lat, a_lng, b_lat, b_lng):
+    """
+    haversine: calculate great circle distance between two points on earth in km
+    """
+
+    R = 6371     # earth radius in km
+
+    a_lat = math.radians(a_lat)
+    a_lng = math.radians(a_lng)
+    b_lat = math.radians(b_lat)
+    b_lng = math.radians(b_lng)
+
+    d_lat = b_lat - a_lat
+    d_lng = b_lng - a_lng
+
+    a = math.pow(math.sin(d_lat / 2), 2) + math.cos(a_lat) * math.cos(b_lat) * math.pow(math.sin(d_lng / 2), 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    return R * c
 
 #%%
 
 gmaps = googlemaps.Client(key=API_KEY)
 
+cities = pd.read_csv('data/cities_edited.csv')
+existing_pois = pd.read_csv(RESULT_FILE)
+existing_place_ids = set(existing_pois.place_id)
+
+try:
+    with open(QUERIED_FILE, mode='rb') as f:
+        queried_cities = pickle.load(f)
+except FileNotFoundError:
+    queried_cities = []
+
 #%%
 
-cities = pd.read_csv('data/cities.csv')
-existing_pois = pd.read_csv('data/places_of_interest.csv')
-existing_place_ids = set(existing_pois.place_id)
 resultrows = []
+for city_i, cityrow in cities.iterrows():
+    print('> city %d/%d: %s' % (city_i+1, len(cities), cityrow.city))
+    for place_query in PLACE_SEARCHES:
+        full_query = place_query + ' in ' + cityrow.city + ', ' + cityrow.country
+        print('>> query: "%s" in lat=%.4f, lng=%.4f' % (full_query, cityrow.lat, cityrow.lng))
 
-cityrow = cities.loc[cities.city == 'Berlin', :].iloc[0]
-cityrow
+        query_id = cityrow.city + cityrow.country + place_query
+        if query_id in queried_cities:
+            print('>> skipping (already queried this city for this kind of places)')
 
-logger.info('> city: %s' % cityrow.city)
-for place_query in PLACE_SEARCHES:
-    logger.info('>> query: %s' % place_query)
-
-    # existing_places_mask = ((existing_pois.city == cityrow.city) &
-    #                         (existing_pois.country == cityrow.country) &
-    #                         (existing_pois['query'] == place_query))
-    #
-    # if sum(existing_places_mask) > 0:
-    #     logger.info('>> skipping (already fetched %d places for this city and query)' % sum(existing_places_mask))
-    #     # TODO
-    #     continue
-
-    places = gmaps.places(query=place_query, location=(cityrow.lat, cityrow.lng), radius=PLACE_SEARCH_RADIUS)
-
-    if places['status'] != 'OK':
-        logger.warning('>> skipping (bad status: %s)' % places['status'])
-        continue
-
-    logger.info('>> got %d results' % len(places['results']))
-    n_pois = 0
-    for place in places['results']:
-        logger.info('>>> place: %s' % place['name'])
-
-        if place['place_id'] in existing_place_ids:
-            logger.info('>> skipping (already queried place with ID %s)' % place['place_id'])
             # don't forget to re-add to results
-            resultrows.append(existing_pois.loc[existing_pois.place_id == place['place_id'], :].iloc[0].to_list())
+            existing_rows = existing_pois.loc[(existing_pois.city == cityrow.city) &
+                                              (existing_pois.country == cityrow.country) &
+                                              (existing_pois['query'] == place_query), :]
+
+            if len(existing_rows) > 0:
+                resultrows.extend([row.to_list() for _, row in existing_rows.iterrows()])
+
             continue
 
-        poptimes = populartimes.get_id(api_key=API_KEY, place_id=place['place_id'])
+        places = gmaps.places(query=full_query, location=(cityrow.lat, cityrow.lng), radius=PLACE_SEARCH_RADIUS)
 
-        if 'current_popularity' in poptimes and 'populartimes' in poptimes:
-            logger.info('>>>> adding this place as place of interest')
-            now = datetime.now()
-            resultrows.append(cityrow.to_list() + [
-                place_query,
-                place['place_id'],
-                place['name'],
-                place['formatted_address'],
-                place['geometry']['location']['lat'],
-                place['geometry']['location']['lng'],
-                # poptimes['current_popularity'],
-                # poptimes['populartimes'][now.weekday()]['data'][now.hour]       # corresponding "usual" popularity
-            ])
-            n_pois += 1
+        if places['status'] != 'OK':
+            print('>> skipping (bad status: %s)' % places['status'])
+            continue
 
-    logger.info('>> got %d places of interest for this city and query' % n_pois)
+        print('>> got %d results' % len(places['results']))
+        queried_cities.append(query_id)
 
-#%%
+        n_pois = 0
+        for i_place, place in enumerate(places['results']):
+            if i_place >= LIMIT_NUM_PLACES:
+                break
 
-logger.info('got %d places of interest in total' % len(resultrows))
+            print('>>> place: %s' % place['name'])
 
-logger.info('preparing and storing dataset')
+            place_lat, place_lng = place['geometry']['location']['lat'], place['geometry']['location']['lng']
+            dist = haversine(cityrow.lat, cityrow.lng, place_lat, place_lng)
+            if dist > (PLACE_SEARCH_RADIUS / 1000) * 2:    # accept larger radius here
+                print('>> found place is out of search radius (distance is %.2f)' % dist)
+                continue
 
-places_of_interest = pd.DataFrame(resultrows, columns=cities.columns.to_list() + [
-    'query', 'place_id', 'name', 'addr', 'place_lat', 'place_lng'
-])
+            if place['place_id'] in existing_place_ids:
+                print('>> skipping (already queried place with ID %s)' % place['place_id'])
+                # don't forget to re-add to results
+                resultrows.append(existing_pois.loc[existing_pois.place_id == place['place_id'], :].iloc[0].to_list())
+                continue
 
-places_of_interest.to_csv(RESULT_FILE, index=False)
+            poptimes = populartimes.get_id(api_key=API_KEY, place_id=place['place_id'])
+
+            if 'current_popularity' in poptimes and 'populartimes' in poptimes:
+                print('>>>> adding this place as place of interest')
+                now = datetime.now()
+
+                resultrows.append(cityrow.to_list() + [
+                    place_query,
+                    place['place_id'],
+                    place['name'],
+                    place['formatted_address'],
+                    place['geometry']['location']['lat'],
+                    place['geometry']['location']['lng'],
+                    # poptimes['current_popularity'],
+                    # poptimes['populartimes'][now.weekday()]['data'][now.hour]       # corresponding "usual" popularity
+                ])
+
+                existing_place_ids.add(place['place_id'])
+                n_pois += 1
+
+        print('>> got %d places of interest for this city and query' % n_pois)
+
+    print('got %d places of interest so far' % len(resultrows))
+
+    print('preparing and storing dataset')
+
+    places_of_interest = pd.DataFrame(resultrows, columns=cities.columns.to_list() + [
+        'query', 'place_id', 'name', 'addr', 'place_lat', 'place_lng'
+    ])
+
+    places_of_interest.to_csv(RESULT_FILE, index=False)
+
+    with open(QUERIED_FILE, mode='wb') as f:
+        pickle.dump(queried_cities, f)
